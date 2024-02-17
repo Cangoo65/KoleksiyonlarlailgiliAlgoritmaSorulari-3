ae10807da7115dfc86c5338b8f69ffe89df21f43
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-17 19:55:23 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+using System.Collections;
+
+Console.Write("Lutfen bir cumle giriniz: ");
+string s = Console.ReadLine();
+s = s.ToLower();
+
+ArrayList list = new ArrayList();
+foreach (var item in s)
+{
+    if (item == 'a' || item == 'e' || item == 'ı' || item == 'i' || item == 'o' || item == 'ö' || item == 'u' || item == 'ü')
+    {
+        list.Add(item);
+    }
+}
+
+Console.WriteLine("Dizinin sesli elemanlari: ");
+foreach (var item in list)
+    Console.Write(item + "-");
